e069f50bdfd30dd8d38ae834fce0253215b6790f
Андрей <w.a.n.d.e.t.19@gmail.com>
2022-04-21 21:02:41 +0300

    Edit bag local

--- site/yoga/public/js/lang.js
+++ site/yoga/public/js/lang.js
@@ -2,5 +2,10 @@ let lang = document.getElementById('lang');
 lang.onchange = function(){
     let url = document.URL;
     let new_url = url.split("_")
-    window.location = new_url[0]+"_"+lang.options[lang.options.selectedIndex].value
+    console.log(new_url[0])
+    if(new_url[0] === 'http://hello.loc:8085/'){
+         window.location = new_url[0]+"info_"+lang.options[lang.options.selectedIndex].value
+    }else {
+        window.location = new_url[0]+"_"+lang.options[lang.options.selectedIndex].value
+    }
 }
